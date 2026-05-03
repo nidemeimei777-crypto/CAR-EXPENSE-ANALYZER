717eeef8c9c89c4ba08561883747e9f6a72bf4e2
nidemeimei777-crypto <nidemeimei777@gmail.com>
2026-05-03 19:15:54 +0300

    Update __init__.py

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,8 +1,1 @@
-from .schemas import (
-    CarCreate, CarUpdate, CarResponse,
-    FuelUpCreate, FuelUpUpdate, FuelUpResponse,
-    ExpenseCreate, ExpenseUpdate, ExpenseResponse,
-    ReminderCreate, ReminderUpdate, ReminderResponse,
-    UserCreate, UserResponse, Token,
-    OdometerReadingCreate, OdometerReadingUpdate, OdometerReadingResponse
-)
+
