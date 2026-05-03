a909159e6b9ff2723714c42d0a0267e2238fa473
nidemeimei777-crypto <nidemeimei777@gmail.com>
2026-05-03 12:41:52 +0300

    Create db.py

---
+++ app/db.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import declarative_base
+from app.config import settings
+
+ASYNC_DB_URL = f"postgresql+asyncpg://{settings.db_username}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
+async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+AsyncSessionLocal = async_sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
+
+Base = declarative_base()
+
+async def get_async_session():
+    async with AsyncSessionLocal() as session:
+        yield session
