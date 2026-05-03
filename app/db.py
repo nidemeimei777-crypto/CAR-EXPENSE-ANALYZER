7d9de465b0e88153a8b221657c87dc2aca1fc39a
nidemeimei777-crypto <nidemeimei777@gmail.com>
2026-05-03 19:10:35 +0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -4,9 +4,12 @@ from app.config import settings
 
 ASYNC_DB_URL = f"postgresql+asyncpg://{settings.db_username}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
 async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
-AsyncSessionLocal = async_sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
+AsyncSessionLocal = async_sessionmaker(
+    async_engine, class_=AsyncSession, expire_on_commit=False
+)
 
 Base = declarative_base()
+
 
 async def get_async_session():
     async with AsyncSessionLocal() as session:
